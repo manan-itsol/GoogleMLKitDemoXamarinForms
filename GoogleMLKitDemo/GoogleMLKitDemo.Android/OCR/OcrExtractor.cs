a0417a17ae1086fbf4a7a7905609de8ad907c1d1
manan-itsol <abdulmannan9669@gmail.com>
2021-08-01 16:55:31 +0500

    improvements and removing redundent views

--- GoogleMLKitDemo/GoogleMLKitDemo.Android/OCR/OcrExtractor.cs
+++ GoogleMLKitDemo/GoogleMLKitDemo.Android/OCR/OcrExtractor.cs
@@ -26,6 +26,7 @@ namespace GoogleMLKitDemo.Droid.OCR
 {
     public class OcrExtractor : IOcrExtractor
     {
+        #region service methods
         public string ProcessImage(byte[] imageData)
         {
             try
@@ -44,10 +45,12 @@ namespace GoogleMLKitDemo.Droid.OCR
                 throw ex;
             }
         }
+        #endregion service methods
 
+        #region helpers methods
         private string ProcessText(SparseArray textBlocks)
         {
-            Dictionary<int, List<(int x, int y, string text)>> ss = new Dictionary<int, List<(int x, int y, string text)>>();
+            List<TextExtractionModel> textExtractions = new List<TextExtractionModel>();
             for (int index = 0; index < textBlocks.Size(); index++)
             {
                 TextBlock tBlock = (TextBlock)textBlocks.ValueAt(index);
@@ -55,39 +58,139 @@ namespace GoogleMLKitDemo.Droid.OCR
                 {
                     var x = line.BoundingBox.CenterX();
                     var y = line.BoundingBox.CenterY();
-                    if (ss.Count == 0)
+                    if (textExtractions.Count == 0)
                     {
-                        ss.Add(y, new List<(int x, int y, string text)> { (x, y, line.Value) });
+                        textExtractions.Add(new TextExtractionModel
+                        {
+                            LinesList = new List<LineWithXY>
+                            {
+                                new LineWithXY(x,y,line.Value)
+                            }
+                        });
                     }
                     else
                     {
-                        var last = ss.LastOrDefault();
-                        if (y >= last.Key - 15 && y <= last.Key + 15)
+                        var nearest = GetNearest(textExtractions, y);
+                        if (y >= nearest.CenterY - 15 && y <= nearest.CenterY + 15)
                         {
-                            ss.Where(x => x.Key == last.Key).FirstOrDefault().Value.Add((x, y, line.Value));
+                            textExtractions.FirstOrDefault(x => x.CenterY == nearest.CenterY).LinesList.Add(new LineWithXY(x, y, line.Value));
                         }
                         else
                         {
-                            if (ss.Any(x => x.Key == y))
+                            if (textExtractions.Any(x => x.CenterY == y))
                             {
-                                ss.Where(x => x.Key == y).FirstOrDefault().Value.Add((x, y, line.Value));
+                                textExtractions.FirstOrDefault(x => x.CenterY == y).LinesList.Add(new LineWithXY(x, y, line.Value));
                             }
                             else
                             {
-                                ss.Add(y, new List<(int x, int y, string text)> { (x, y, line.Value) });
+                                textExtractions.Add(new TextExtractionModel
+                                {
+                                    LinesList = new List<LineWithXY>
+                                    {
+                                        new LineWithXY(x,y,line.Value)
+                                    }
+                                });
                             }
                         }
                     }
                 }
             }
+            textExtractions = MergeNearestRows(textExtractions);
             string finalLines = string.Empty;
-            int count = 1;
-            foreach (var item in ss.OrderBy(x => x.Key))
+            foreach (var item in textExtractions.OrderBy(x => x.CenterY))
             {
-                finalLines = $"{finalLines}Row{count}: {string.Join(' ', item.Value.OrderBy(x => x.x).Select(x => x.text).ToList())}\n";
-                count++;
+                finalLines = $"{finalLines}{string.Join(' ', item.LinesList.OrderBy(x => x.X).Select(x => x.Text).ToList())}\n";
             }
             return finalLines;
         }
+
+        private TextExtractionModel GetNearest(List<TextExtractionModel> textExtractions, int currentKey)
+        {
+            var sorted = textExtractions.OrderBy(x => x.CenterY).ToList();
+            TextExtractionModel last = null;
+            foreach (var item in sorted)
+            {
+                var less = currentKey < item.CenterY;
+                if (less)
+                {
+                    last = item;
+                }
+                else
+                {
+                    if (last == null)
+                        return item;
+                    var lessDiff = currentKey - last.CenterY;
+                    var greaterDiff = item.CenterY - currentKey;
+                    if (lessDiff < greaterDiff)
+                        return last;
+                    else
+                        return item;
+                }
+            }
+            return last;
+        }
+
+        private List<TextExtractionModel> MergeNearestRows(List<TextExtractionModel> textExtractions)
+        {
+            textExtractions = textExtractions.OrderBy(x => x.CenterY).ToList();
+            List<TextExtractionModel> toRemove = new List<TextExtractionModel>();
+            TextExtractionModel last = null;
+            foreach (var current in textExtractions)
+            {
+                if (last == null)
+                {
+                    last = current;
+                    continue;
+                }
+                var diff = current.CenterY - last.CenterY;
+                if (diff <= 20)
+                {
+                    current.LinesList.AddRange(last.LinesList);
+                    toRemove.Add(last);
+                }
+                last = current;
+            }
+            foreach (var item in toRemove)
+            {
+                textExtractions.Remove(item);
+            }
+            return textExtractions;
+        }
+        #endregion helpers methods
+    }
+
+    public class TextExtractionModel
+    {
+        public TextExtractionModel()
+        {
+            LinesList = new List<LineWithXY>();
+        }
+
+        public float CenterY
+        {
+            get
+            {
+                float avg = 0;
+                if (LinesList != null && LinesList.Count > 0)
+                {
+                    avg = LinesList.Sum(x => x.Y) / LinesList.Count;
+                }
+                return avg;
+            }
+        }
+        public List<LineWithXY> LinesList { get; set; }
+    }
+
+    public class LineWithXY
+    {
+        public LineWithXY(int x, int y, string text)
+        {
+            X = x;
+            Y = y;
+            Text = text;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Text { get; set; }
     }
 }
--- GoogleMLKitDemo/GoogleMLKitDemo/ViewModels/BaseViewModel.cs
+++ GoogleMLKitDemo/GoogleMLKitDemo/ViewModels/BaseViewModel.cs
@@ -1,5 +1,4 @@
-﻿using GoogleMLKitDemo.Models;
-using GoogleMLKitDemo.Services;
+﻿using GoogleMLKitDemo.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,8 +9,6 @@ namespace GoogleMLKitDemo.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
-        public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
-
         bool isBusy = false;
         public bool IsBusy
         {
